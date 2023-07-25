0b835eb4a2b5c77ba43d0e6652afff07490be842
221561163 <140560510+221561163@users.noreply.github.com>
2023-07-25 23:58:28 +0200

    Add files via upload

---
+++ BasicAppFrame.java
@@ -0,0 +1,14 @@
+
+import za.ac.ui.MyExampleClasss;
+
+
+public class BasicAppFrame
+{
+    public static void main(String[] args)
+    {
+      MyExampleClasss myFrame;
+    
+      myFrame = new MyExampleClasss();
+    }
+    
+}
